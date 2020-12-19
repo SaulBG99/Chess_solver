cfdc0d33facbfbb7bb31b7d92493a18749c85888
SaulBG99 <a01282698@itesm.mx>
2020-12-18 21:36:40 -0600

    Init

--- ChessSolver.java
+++ ChessSolver.java
@@ -4,17 +4,32 @@ public class ChessSolver
 {
  public static void main (String[] args) throws IOException
    {
-   //Chessgame ss= new Chessgame();
+   Chessgame ss= new Chessgame();
    boolean endGame = false;
    String input = "";
    while(!endGame){
     try {
       BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
-      System.out.println("Write next command")
+      System.out.println("Write next command");
       input = reader.readLine();
-      if(input == "end"){
+      if(input.equals("end")){
         endGame = true;
-      }
+      }else if(input.equals("help") || input.equals("h")){
+        System.out.println( "\nList of possible commands" );
+        System.out.println( "end -> Terminate the play\n" );
+
+      }else if(input.equals("restart") || input.equals("r")){
+        ss = new Chessgame();
+
+      }else if(input.equals("display") || input.equals("d")){
+        ss.display();
+
+      }else if(input.equals("take") || input.equals("t")){ //As in "take recommendation"
+        ss.command(ss.recommendation());
+
+      }else{
+        ss.command(input);
+      }      
 			}
 		catch( Exception e ){
 			System.out.println( "An exception occured!" );
---
+++ Chessgame.java
@@ -0,0 +1,17 @@
+public class Chessgame {
+    public Chessgame(){
+
+    }
+    public void command(String cmd){
+        System.out.println("Not yet");
+    }
+    public void commandAnalisis(String cmd){
+        
+    }
+    public String recommendation(){
+        return "Move queen";
+    }
+    public void display(){
+        System.out.println("Not displaying");
+    }
+}
